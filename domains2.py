461aa1fc43b85c81349f068e1bc23c1b29837f9c
Roman Ch <hello@romangagarin.com>
2018-01-14 23:58:25 +0000

    project changes

--- domains2.py
+++ domains2.py
@@ -1,22 +1,56 @@
 import whois
 from datetime import datetime, timedelta
+import requests
 
-f = open('domains.txt','r')
-urls = f.readlines()
-domains = []
-for line in urls:
-    domains.append(line)
-f.close()
 
-for dom in domains:
+def load_list_of_domains():
+    f = open('domains.txt', 'r')
+    urls = f.readlines()
+    domains = []
+    for line in urls:
+        domains.append(line.strip())
+    f.close()
+    print(domains)
+    return domains
+
+
+def check_200(domains):
+    for dom in domains:
+        print(dom)
+        r = requests.get(dom)
+        print(r.status_code)
+        print(check_if_expiration_date_is_ok(dom))
+
+
+def check_if_expiration_date_is_ok(dom):
     time_now = datetime.now()
     details = whois.whois(dom)
     domain_expiration_date = details.expiration_date
-
-    if domain_expiration_date - datetime.now() >= timedelta(days=31):
+    if domain_expiration_date - time_now >= timedelta(days=31):
         print('ok')
     else:
         print('not ok')
+
+
+# def check_if_expiration_date_is_ok(domains):
+#     for dom in domains:
+#         time_now = datetime.now()
+#         details = whois(dom)
+#         domain_expiration_date = details.expiration_date
+#         print(domain_expiration_date - time_now)
+#         print(domain_expiration_date)
+#         print(time_now)
+#         print(timedelta(days=31))
+#         if domain_expiration_date - time_now >= timedelta(days=31):
+#             print('ok')
+#         else:
+#             print('not ok')
+
+if __name__ == '__main__':
+    domains = load_list_of_domains()
+    output = check_200(domains)
+
+
 
 
 
@@ -28,6 +62,7 @@ for dom in domains:
 # print(domain_expiration_date)
 # time_now = datetime.now()
 # print(time_now)
+# print(domain_expiration_date - datetime.now())
 # if domain_expiration_date - datetime.now() >= timedelta(days=31):
 #     print('ok')
 # else:
