54b26c08c2ba7d49d602a991acbeab561bcdbdc6
Roman Ch <hello@romangagarin.com>
2018-02-03 17:01:37 +0000

    minor tweaks

--- check_sites_health.py
+++ check_sites_health.py
@@ -5,13 +5,17 @@ import requests
 
 
 def load_urls_list(path):
-    with open(path, 'r') as domains:
-        return domains.read().splitlines()
+    with open(path, 'r') as file_with_urls:
+        return file_with_urls.read().splitlines()
 
 
 def is_server_respond_ok(domain):
-    response = requests.get(domain)
-    return response.ok
+    try:
+        response = requests.get(domain)
+    except requests.exceptions.RequestException:
+        return False
+    else:
+        return response.ok
 
 
 def is_expiration_in_month(date_time, number_of_days_in_month):
@@ -32,11 +36,11 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.exit('Usage: python3 check_sites_health.py <path_to_txt>')
     filepath = sys.argv[1]
-    websites = load_urls_list(filepath)
-    for website in websites:
-        print('Checking {}'.format(website))
+    urls = load_urls_list(filepath)
+    for url in urls:
+        print('Checking {}'.format(url))
         print('\tServer respond with 200: {}'.format(
-            is_server_respond_ok(website)))
-        expiration_date = get_domain_expiration_date(website)
+            is_server_respond_ok(url)))
+        expiration_date = get_domain_expiration_date(url)
         print('\tExpiring in month: {}\n'.format(
             is_expiration_in_month(expiration_date, days_in_calendar_month)))
